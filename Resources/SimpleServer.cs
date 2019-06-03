de7c3789bd743444a68bc675a6cb783c6bbd9c11
angad <singh21.h@gmail.com>
2019-06-02 20:12:57 -0400

    Added Simple server class. for single threaded debugging and testing.

---
+++ Resources/SimpleServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+
+public class SimpleServer
+{
+
+    private const int BUFFSIZE_DEFAULT = 32;
+
+    private const int DEFAULT_PORT = 8080;
+
+    private int Port { get; set; }
+
+    private Socket Socket = null;
+
+    public SimpleServer(int port)
+	{
+
+        Port = port;
+
+        try {
+
+
+
+        }
+
+        catch (Exception e) {
+
+
+        }
+
+	}
+
+
+
+}
